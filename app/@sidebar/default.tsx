2973cbbdf52eee205a9dd962cfa41b8d707b11d7
2001Y <15167896+2001Y@users.noreply.github.com>
2025-03-07 09:25:32 +0000

    Deploy: 2025-03-07-09-25-32

--- app/@sidebar/default.tsx
+++ app/@sidebar/default.tsx
@@ -1,10 +1,13 @@
 import { Suspense } from "react";
 import { SidebarWrapper } from "../components/SidebarWrapper";
+import { ContextMenuProvider } from "../context/ContextMenuContext";
 
 export default async function SidebarPage() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <SidebarWrapper />
+      <ContextMenuProvider>
+        <SidebarWrapper />
+      </ContextMenuProvider>
     </Suspense>
   );
 }
