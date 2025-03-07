d40ba6da16b705d77433d0852107fdab13666cb3
2001Y <15167896+2001Y@users.noreply.github.com>
2025-03-07 09:36:24 +0000

    Deploy: 2025-03-07-09-36-24

--- app/@sidebar/default.tsx
+++ app/@sidebar/default.tsx
@@ -1,10 +1,16 @@
 import { Suspense } from "react";
 import { SidebarWrapper } from "../components/SidebarWrapper";
+import { TreeProvider } from "../context/TreeContext";
+import { FileTreeServer } from "../components/FileTreeServer";
 
 export default async function SidebarPage() {
+  const { initialTree, updateTree } = await FileTreeServer();
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <SidebarWrapper />
+      <TreeProvider initialTree={initialTree} updateTree={updateTree}>
+        <SidebarWrapper />
+      </TreeProvider>
     </Suspense>
   );
 }
